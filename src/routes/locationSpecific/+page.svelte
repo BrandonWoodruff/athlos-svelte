a34228235401599d6acc1d6eeb13236b2bdc5627
BrandonWoodruff <brandonwoodywoodruff@gmail.com>
2024-11-13 19:24:56 -0700

    Update +page.svelte

--- src/routes/locationSpecific/+page.svelte
+++ src/routes/locationSpecific/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onMount } from 'svelte';
     import {goto} from '$app/navigation';
+    import { Button } from '$lib/components/ui/button';
 
     function redirectToContactManagement() {
         goto('/contactManagement');
@@ -30,9 +31,7 @@
                     <label for="pin-location" class="txt-lg font-semibold mb-2 text-decoration-line: underline">Pin Location</label>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6087.525598393933!2d-111.71892992360303!3d40.28101906388739!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x874d9b1faf329a5f%3A0xe25ac48a100272e0!2sFulton%20Library!5e0!3m2!1sen!2sus!4v1730755552754!5m2!1sen!2sus" height="450" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-                <button type="button" on:click={redirectToContactManagement} class="btn btn-secondary rounded-circle" style="width: 50px; height: 50px;">
-                    <i class="fa-sharp fa-solid fa-bell"></i> Contact
-                </button>
+                <Button on:click={redirectToContactManagement}>Contact Management</Button>
             </div>
         </section>
     </main>
