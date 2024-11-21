5848fd8ddaf5d64734987ecbba8814b125050e7f
Keatongull <129561986+Keatongull@users.noreply.github.com>
2024-11-21 11:35:57 -0700

    Moved the contact management button above the pin

--- src/routes/locationSpecific/+page.svelte
+++ src/routes/locationSpecific/+page.svelte
@@ -27,11 +27,11 @@
                     <label for="address" class="txt-lg font-semibold mb-2 text-decoration-line: underline">Address</label>
                     <label for="address" class="txt-lg font-semibold mb-2">1234 Main St.</label>
                 </div>
+                <Button on:click={redirectToContactManagement}>Contact Management</Button>
                 <div class="flex flex-col w-full max-w-md">
                     <label for="pin-location" class="txt-lg font-semibold mb-2 text-decoration-line: underline">Pin Location</label>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6087.525598393933!2d-111.71892992360303!3d40.28101906388739!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x874d9b1faf329a5f%3A0xe25ac48a100272e0!2sFulton%20Library!5e0!3m2!1sen!2sus!4v1730755552754!5m2!1sen!2sus" height="450" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-                <Button on:click={redirectToContactManagement}>Contact Management</Button>
             </div>
         </section>
     </main>
